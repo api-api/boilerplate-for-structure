1b14d04080f02d468db5c639386fb60d14888661
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-29 12:22:42 +0200

    Add some commented-out code to provide an easier starting point when defining a structure.

--- src/Structure_Dummy_Name/Structure_Dummy_Name.php
+++ src/Structure_Dummy_Name/Structure_Dummy_Name.php
@@ -29,7 +29,41 @@ if ( ! class_exists( 'APIAPI\Structure_Dummy_Name\Structure_Dummy_Name' ) ) {
 		 * @access protected
 		 */
 		protected function setup() {
+			// You must set the following data.
+			//$this->title       = 'Title of the API, for example "Google Analytics".';
+			//$this->description = 'Third-person description of what the API does, for example "Views and manages your Google Analytics data.".';
+			//$this->base_uri    = 'Base URI (without any routes) to access the API.';
 
+			// The following must be set if the API supports or partly requires authentication.
+			//$this->authenticator = 'Identifier of a registered authenticator.';
+			//$this->authentication_data_defaults = array(); // What you can set here depends on the specified authenticator.
+
+			// You must specific all available routes and their methods of the API, providing details on how each one works.
+			// The following is a simple example of a single route.
+			/*$this->routes['/account/settings.json'] = array(
+				'methods' => array(
+					'GET'  => array(
+						'description'          => 'Returns settings for the authenticating user.',
+						'needs_authentication' => true,
+						'params'               => array(),
+					),
+					'POST' => array(
+						'description'          => 'Updates the authenticating user’s settings.',
+						'needs_authentication' => true,
+						'params'               => array(
+							'age'  => array(
+								'description' => 'The age of this user.',
+								'type'        => 'integer',
+								'minimum'     => 0,
+							),
+							'lang' => array(
+								'description' => 'The language in which the interface renders for this user.',
+								'type'        => 'string',
+							),
+						),
+					),
+				),
+			);*/
 		}
 	}
 
